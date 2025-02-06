13f34e32618bfd0b95cfe6f64b1a7c889d9360e8
christina-baldwin <christina.baldwin13@yahoo.com>
2025-02-06 11:54:27 +0100

    adding and styling countdown timer, more stylign on dice

--- script.js
+++ script.js
@@ -28,3 +28,35 @@ const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 console.log(currentYear);
 yearEl.textContent = currentYear;
+
+// COUNTDOWN
+const endDate = new Date("02/14/2025");
+const sec = 1000;
+const min = sec * 60;
+const hour = min * 60;
+const day = hour * 24;
+let timer = 0;
+
+function showRemainingTime() {
+  const currentDate = new Date();
+  const distanceDate = endDate - currentDate;
+
+  if (distanceDate < 0) {
+    clearInterval(timer);
+    document.querySelector(".countdown").innerHTML = "RELEASED!";
+
+    return;
+  }
+
+  const days = Math.floor(distanceDate / day);
+  const hours = Math.floor((distanceDate % day) / hour);
+  const mins = Math.floor((distanceDate % hour) / min);
+  const secs = Math.floor((distanceDate % min) / sec);
+
+  document.querySelector(".countdown").innerHTML = days + "days ";
+  document.querySelector(".countdown").innerHTML += hours + "hrs ";
+  document.querySelector(".countdown").innerHTML += mins + "mins ";
+  document.querySelector(".countdown").innerHTML += secs + "sec";
+}
+
+timer = setInterval(showRemainingTime, 1000);
