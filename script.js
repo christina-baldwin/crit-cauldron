135d35358cd64e99d1321f22af53778df62f83d7
christina-baldwin <christina.baldwin13@yahoo.com>
2025-02-06 10:18:32 +0100

    making the product filters work

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// FILTER PRODUCT TYPE
+document.querySelectorAll("[data-filter]").forEach((button) => {
+  button.addEventListener("click", () => {
+    const filter = button.getAttribute("data-filter");
+    document.querySelectorAll(".product").forEach((product) => {
+      if (
+        filter === "all" ||
+        product.getAttribute("data-category") === filter
+      ) {
+        product.style.display = "block";
+      } else {
+        product.style.display = "none";
+      }
+    });
+  });
+});
